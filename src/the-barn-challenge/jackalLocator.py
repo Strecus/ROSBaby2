709b5fcd6b1faeac4772cac048fd31eefa5e414f
Strecus <111085240+Strecus@users.noreply.github.com>
2025-04-29 11:35:38 -0400

    Update jackalLocator.py

--- src/the-barn-challenge/jackalLocator.py
+++ src/the-barn-challenge/jackalLocator.py
@@ -7,20 +7,20 @@ import math
 
 def location():
     """Get the true x, y, yaw (radians) of the Jackal from Gazebo."""
-    print("hey")
+    
     # Wait until model states is available
     data = rospy.wait_for_message('/gazebo/model_states', ModelStates)
-    print("hey")
+    
     try:
         idx = data.name.index('jackal')
     except ValueError:
         rospy.logerr("Jackal not found in /gazebo/model_states!")
         return None, None, None
-    print("hey")
+    
     pose = data.pose[idx]
     x = pose.position.x
     y = pose.position.y
-    print("hey")
+  
     # Convert quaternion to yaw
     orientation = pose.orientation
     (_, _, yaw) = tf.transformations.euler_from_quaternion([
@@ -29,7 +29,7 @@ def location():
         orientation.z,
         orientation.w
     ])
-    print("hey")
+    print("Jackal's position is: (" + x+"," + y+"," +z+")")
     return x, y, yaw
 
 if __name__ == "__main__":
